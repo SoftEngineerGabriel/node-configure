345e3d7c3501cceebfcdf07701d8e747f5a1012b
SoftEngineerGabriel <farias2752@gmail.com>
2023-10-05 07:05:57 -0300

    Imutabilidad3e para trabalhar com os dados da aplicacao

--- Public/git-favorites/js/Favorites.js
+++ Public/git-favorites/js/Favorites.js
@@ -4,6 +4,31 @@
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
+        this.load()
+    }
+
+    load(){
+        this.entries = [{
+            login: 'maykbrito',
+            name: 'Mayk Brito',
+            public_repos: 70,
+            followers: 120000
+        },
+        
+        {
+            login: 'diego3g',
+            name: 'Diego fernandes',
+            public_repos: 76,
+            followers: 120000
+        }
+        ]
+
+        elete(user) {
+            const filteredENtries = this.entries.filter(entry => entry.login !== user.login)
+        
+        console.log(filteredENtries)
+        }
+        
     }
 }
 
@@ -13,18 +38,67 @@ export class Favorites {
 export class FavoritesView extends Favorites{
     constructor(root) {
         super(root)
+        this.tbody = this.root.querySelector('table tbody')
 
         this.update()
     }
 
     update() {
         this.removeAllTr()
+
+        
+
+        this.entries.forEach(user => {
+            const row = this.createRow()
+
+            row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.user img').alt = `Imagem de ${user.name}`
+            row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user span').textContent = user.login
+            row.querySelector('.repositories').textContent = user.public_repos
+            row.querySelector('.followers').textContent = user.followers
+
+
+            row.querySelector('.remove').onclick = () => {
+                const isOK = confirm('Tem certeza que deseja excluir esse usuario?')
+            }
+
+            if(isOK){
+                this.delete(user)
+            }
+
+            this.tbody.append(row)
+        })
+    }
+
+    createRow(){
+
+        const tr = document.createElement('tr')
+
+        tr.innerHTML = `
+            <td class="user">
+                <img src="https://github.com/maykbrito.png" alt="imagem github">
+                <a href="https://github.com/maykbrito" target="_blank">
+                    <p>Mayk Brito</p>
+                    <span>maykbrito</span>
+                </a>
+            </td>
+            <td class="repositories">
+                76
+            </td>
+            <td class="followers">
+                9589
+            </td>
+            <td>
+                <button class="remove">&times;</button>
+            </td>
+        `
+
+        return tr
     }
 
     removeAllTr() {
-        const tbody = this.root.querySelector('table tbody')
-
-        tbody.querySelectorAll('tr').forEach((tr) => {
+        this.tbody.querySelectorAll('tr').forEach((tr) => {
             tr.remove()
         });
     }
